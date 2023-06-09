5f3eb4ae775c4f0c30e2f700f8a7a238a36f987d
SuperExe404 <diegomon0440@gmail.com>
2023-06-09 16:20:50 -0600

    02 Variables

---
+++ variable.py
@@ -0,0 +1,15 @@
+#Phyton en No tipado
+
+name = "Diego"
+last_name = "Montes"
+age = 18
+height = 1.68
+is_student = True
+group = "3C"
+
+#funcion type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(group))
