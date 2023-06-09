35b5d51a74e7afca7be1a2e274ee85747b92d918
SuperExe404 <diegomon0440@gmail.com>
2023-06-09 16:52:25 -0600

    04 Operadores

---
+++ operaciones.py
@@ -0,0 +1,21 @@
+#operacionesmatematicas 
+# + - * / %
+# / divicion float
+# // divicione entera
+# ** potencia
+
+print(12 + 12)
+print(12 - 12)
+print(12 / 12)
+print(12 * 12)
+print(12 % 12)
+print(10 / 3)
+print(10 // 3)
+print(2 ** 16)
+
+#operaciones para string
+# + para concatenar
+# * para replicar
+print("Hola " + "Mundo!")
+print("Hola " * 3)
+print("_______" * 4)
