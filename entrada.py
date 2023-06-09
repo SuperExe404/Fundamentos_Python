3f2761d8352561a0f7e345b6362b8527bbe0e9ec
SuperExe404 <diegomon0440@gmail.com>
2023-06-09 16:43:11 -0600

    03 Entrada de Datos

---
+++ entrada.py
@@ -0,0 +1,22 @@
+#funcion input -> retorna string
+
+num_a = int(input("Ingrese un numero:"))
+num_b = int(input("Ingrese un numero:"))
+
+print(num_a + num_b)
+
+name = input("Ingresa tu nombre:")
+age = int(input("Ingresa tu edad:"))
+city = input("Ingresa tu ciudad/pueblo:")
+
+#string format
+"""
+    Hola, soy name, tengo age años y vivo en city 
+"""
+greeting = "Hola, soy {}, tengo a{} años y vivo en {}"
+
+print(greeting.format(name, age, city))
+
+greeting_2 = f"Hola, soy { name }, tengo { age } años y vivo en { city }"
+
+print(greeting_2)
