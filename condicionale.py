59d349a586f18c009cc182a35f7501763dca8078
SuperExe404 <diegomon0440@gmail.com>
2023-06-13 12:17:25 -0600

    06 Condicionale

---
+++ condicionale.py
@@ -0,0 +1,22 @@
+# if, elif, esle
+# if expresion_boleana:
+#   bloque de codigo 
+
+age = 18
+
+if age >= 18:
+    print("Es mayor de Edad")
+
+if age <18:
+    print("Es menor de Edad")
+else:
+    print("Es mayor de Edad")
+
+age = 75
+
+if age < 18:
+    print("Es menor de Edad")
+elif age < 70:
+    print("Es mayor de Edad")
+else:
+    print("Es un Adulto mayor")
